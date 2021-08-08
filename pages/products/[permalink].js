b7cdd8bc15b869be5524f5e581094477544ea4c9
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-08-08 14:31:57 -0300

    Add to cart

--- pages/products/[permalink].js
+++ pages/products/[permalink].js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import commerce from '../../lib/commerce';
+import { useCartDispatch } from '../../context/cart';
 
 export async function getStaticProps({params}) {
     const {permalink} = params
@@ -29,10 +30,15 @@ export async function getStaticPaths() {
 }
 
 export default function ProductPage({product}) {
+    const { ...setCart } = useCartDispatch()
+
+    const addToCart = () => commerce.cart.add(product.id).then(({ cart }) => setCart(cart))
+    
     return (
         <React.Fragment>
             <h1>{product.name}</h1>
             <p>{product.price.formatted_with_symbol}</p>
+            <button onClick={addToCart}>Add to cart</button>
         </React.Fragment>
     )
 }
